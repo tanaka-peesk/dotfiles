79c1b37dd73bd4b00c159ab5b4e777abe6deed02
tanaka-peesk <tanaka.peesk@gmail.com>
2017-09-27 20:03:30 +0900

    remove .bashrc

--- installer.sh
+++ installer.sh
@@ -1,10 +1,6 @@
-if [ -e ~/.bashrc ]; then
-    mv ~/.bashrc ~/.bashrc.org
-fi
 if [ -e ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.org
 fi
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.vim/dein ~/.vim/cache
 wget -P ~/ https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh
